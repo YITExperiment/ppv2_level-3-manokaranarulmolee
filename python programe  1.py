3fbbbc748adfde572a37a525a3951328383f9900
manokaranarulmolee <134794885+manokaranarulmolee@users.noreply.github.com>
2023-05-27 21:24:49 +0530

    Add files via upload

---
+++ python programe  1.py
@@ -0,0 +1,15 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/TAB-ST-50/AppData/Local/Programs/Python/Python311/szagani 1.py
+Heiio,World!
+>>> 
+= RESTART: C:/Users/TAB-ST-50/AppData/Local/Programs/Python/Python311/szagani 1.py
+Heiio,World!
+>>> 
+= RESTART: C:/Users/TAB-ST-50/AppData/Local/Programs/Python/Python311/szagani 1.py
+Heiio,World!
+What is your name?szahani
+Hello ?szahani
+>>> 
+================================ RESTART: Shell ================================
