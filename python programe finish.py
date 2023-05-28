922a460051b1cf54c8f087d6317e479522c04b19
manokaranarulmolee <134794885+manokaranarulmolee@users.noreply.github.com>
2023-05-28 15:01:28 +0530

    Add files via upload

---
+++ python programe finish.py
@@ -0,0 +1,10 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+================================ RESTART: C:/Users/TAB-ST-50/AppData/Local/Programs/Python/Python311/python programme.py ===============================
+Hello,World!
+>>> 
+===================================== RESTART: C:/Users/TAB-ST-50/AppData/Local/Programs/Python/Python311/py 1.1.py ====================================
+Hello,World
+What is your name?szahani
+Hello,szahani
