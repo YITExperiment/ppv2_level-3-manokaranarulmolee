d63166097cad658d9024986e9f902a92b93ed905
manokaranarulmolee <134794885+manokaranarulmolee@users.noreply.github.com>
2023-06-27 17:46:20 +0530

    variable assiginig

---
+++ main.py
@@ -0,0 +1,12 @@
+Python 3.11.3 (tags/v3.11.3:f3909b8, Apr  4 2023, 23:49:59) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> weight=41kg
+SyntaxError: invalid decimal literal
+>>> weight=41
+>>> print(weight)
+41
+>>> 
+>>> height=16.58
+>>> print(height)
+16.58
